b41ebb2f1b24edc98daefd5657b4e31837725eb0
Anthony Okonta <okontaa@gmail.com>
2023-05-30 07:03:51 +0200

    chore: add lazy to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Suspense, lazy } from 'react';
 import './App.css';
-
+const Chat=lazy(()=>import('./Pages/Chat'));
+const Loading =lazy(()=>import('./Components/ui/Loading'))
+document.title="Doddle Chat"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<Loading/>}>
+    <Chat/>
+    </Suspense>
   );
 }
 
