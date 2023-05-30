447c8ca6f47468e75090cc420d19d7e7495f15c0
Anthony Okonta <okontaa@gmail.com>
2023-05-30 15:30:51 +0200

    chore: add sender chat tile, format chart card, format fetch api,Add tests

--- src/Components/ui/Loading/Loading.test.tsx
+++ src/Components/ui/Loading/Loading.test.tsx
@@ -1,1 +1,11 @@
-import {render,screen} from '@testing-library/react'
+import { render, screen } from "@testing-library/react"
+import Loading from "./Loading"
+
+describe("Loading component", () => {
+  test("it should check if the loading text is inside the component ", () => {
+    const text = "Loading..."
+    render(<Loading />)
+    const linkElement = screen.getByText(text)
+    expect(linkElement).toBeInTheDocument()
+  })
+})
